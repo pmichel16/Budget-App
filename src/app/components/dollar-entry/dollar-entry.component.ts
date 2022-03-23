2028ef625b06dfc15d03c0c9b8167d163de9c1e6
Polly Michel <47877075+pmichel16@users.noreply.github.com>
2022-03-22 21:37:27 -0500

    Added ability to edit the dollar textboxes and updated side bar text

--- src/app/components/dollar-entry/dollar-entry.component.ts
+++ src/app/components/dollar-entry/dollar-entry.component.ts
@@ -16,6 +16,18 @@ export class DollarEntryComponent implements OnInit {
     //Always filter out characters that aren't digits or decimal points
     //TODO: Only allow one decimal point
     event.target.value = event.target.value.replace(/[^\d\.]/g, "");
+    //Only allow one decimal point, and 2 characters after the decimal
+    let splitStr:string[] = event.target.value.split('.');
+    if(splitStr.length > 2) {
+      let result = splitStr[0]+"."+splitStr[1];
+      splitStr = splitStr.slice(2);
+      splitStr.forEach(s => result += s);
+      event.target.value = result;
+    }
+    var index = event.target.value.indexOf('.')
+    if(index > -1) {
+      event.target.value = event.target.value.substr(0, index + 3);
+    }
 
     //If the entry doesn't start with a $, then prepend the $ sign
     let pattern = /^\$/;
